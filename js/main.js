9f7c0226e030df06486e5b5b4342190a03fe730b
yamanekaito <yamanekaito0702@gmail.com>
2020-06-17 05:07:15 +0900

    lesson6

--- js/main.js
+++ js/main.js
@@ -13,16 +13,44 @@
       this.width = canvas.width;
       this.height = canvas.height;
       this.r = 60;
+      this.angle = 0;
     }
 
     draw() {
+      this.ctx.save();
+
+      this.ctx.translate(this.width / 2,this.height /2);
+      //原点の位置を移動　指定した位置を(0,0)にする 
+      this.ctx.rotate(Math.PI / 180 * this.angle);
+
+      // this.ctx.beginPath();
+      // // this.ctx.arc(this.width / 2,this.height /2, this.r,0,2 * Math.PI);
+      // this.ctx.arc(0,0, this.r,0,2 * Math.PI);
+      // this.ctx.stroke();
+
       this.ctx.beginPath();
-      this.ctx.arc(this.width / 2,this.height /2, this.r,0,2 * Math.PI);
+      // this.ctx.moveTo(this.width / 2,this.height /2 - this.r -5);
+      // this.ctx.lineTo(this.width / 2,this.height /2 - this.r + 5);
+      this.ctx.moveTo(0, - this.r -5);
+      this.ctx.lineTo(0,- this.r + 5);
+      this.ctx.strokeStyle = 'orange';
+      this.ctx.lineWidth = 6;
       this.ctx.stroke();
+
+      this.ctx.restore();
+    }
+
+    update() {
+      this.angle += 12; 
     }
 
     run() {
+      this.update();
       this.draw();
+
+      setTimeout(() => {
+        this.run();
+      },100);
     }
   }
   
